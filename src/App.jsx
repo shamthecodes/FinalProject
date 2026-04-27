d4d99d101c564877e914618630bb5e793765ff30
shamthecodes <shamthecoder@gmail.com>
2026-04-27 11:39:58 +0530

    update  project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 const App = () => {
-  return <div>Saku manage nadi</div>;
+  return <div>Saku manage nadi mekidu seguvanthe</div>;
 };
 
 export default App;
