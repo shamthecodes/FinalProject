6c325c5705c6b0ac1f9b568927e6fc143e3d9a27
shamthecodes <shamthecoder@gmail.com>
2026-04-28 10:15:29 +0530

    tailwind  project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import "./App.css";
 const App = () => {
-  return <div>Saku manage nadi mekidu seguvanthe</div>;
+  return (
+    <div className="flex items-center justify-center h-screen bg-black">
+      <h1 className="text-4xl font-bold text-green-400">Tailwind Working</h1>
+    </div>
+  );
 };
 
 export default App;
