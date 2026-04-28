a1476435370bcdccbaa5f6c67c77be47d668ec40
shamthecodes <shamthecoder@gmail.com>
2026-04-28 20:51:19 +0530

    ntg  project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import React from "react";
 import "./App.css";
+import Hero from "./components/Hero";
 const App = () => {
-  return (
-    <div className="flex items-center justify-center h-screen bg-black">
-      <h1 className="text-4xl font-bold text-green-400">Tailwind Working</h1>
-    </div>
-  );
+  return <Hero />;
 };
 
 export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/react";
+import React from "react";
+
+const Hero = () => {
+  return (
+    <>
+      <header>heelo</header>
+    </>
+  );
+};
+
+export default Hero;
